e4ad7d5ea0d9359888cc507ba5f2ae2ef2cc55d6
Sayan Mallick <snyxmk@gmail.com>
2022-09-09 15:06:34 +0530

    Basic Logging

--- src/startup.rs
+++ src/startup.rs
@@ -1,5 +1,6 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -9,6 +10,7 @@ pub fn run(listener: TcpListener, connection: PgPool) -> Result<Server, std::io:
 
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
