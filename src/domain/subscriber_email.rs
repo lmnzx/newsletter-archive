d98a29a0243a87fbd10084d2eb79685950b56dbc
Sayan Mallick <snyxmk@gmail.com>
2022-09-15 20:03:45 +0530

    Unit Testing for valid name and email

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -18,3 +18,49 @@ impl AsRef<str> for SubscriberEmail {
         &self.0
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::SubscriberEmail;
+    use claim::assert_err;
+    use fake::faker::internet::en::SafeEmail;
+    use fake::Fake;
+
+    // #[derive(Debug, Clone)]
+    // struct ValidEmailFixture(pub String);
+
+    // impl quickcheck::Arbitrary for ValidEmailFixture {
+    //     fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+    //         let email = SafeEmail().fake_with_rng(g);
+    //         Self(email)
+    //     }
+    // }
+    // #[quickcheck_macros::quickcheck]
+    // fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+    //     SubscriberEmail::parse(valid_email.0).is_ok()
+    // }
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let email = "".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_at_symbol_is_rejected() {
+        let email = "ursuladomain.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_subject_is_rejected() {
+        let email = "@domain.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn vaild_email_are_parsed_successfully() {
+        let email = SafeEmail().fake();
+        claim::assert_ok!(SubscriberEmail::parse(email));
+    }
+}
--- src/domain/subscriber_name.rs
+++ src/domain/subscriber_name.rs
@@ -23,3 +23,47 @@ impl AsRef<str> for SubscriberName {
         &self.0
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::SubscriberName;
+    use claim::{assert_err, assert_ok};
+
+    #[test]
+    fn a_256_grapheme_long_name_is_vaild() {
+        let name = "e".repeat(256);
+        assert_ok!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn a_name_longer_than_256_graphemes_is_rejected() {
+        let name = "a".repeat(257);
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn whitespace_only_names_are_rejected() {
+        let name = " ".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let name = "".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn names_containing_an_invalid_character_are_rejected() {
+        for name in &['/', '(', ')', '"', '<', '>', '\\', '{', '}'] {
+            let name = name.to_string();
+            assert_err!(SubscriberName::parse(name));
+        }
+    }
+
+    #[test]
+    fn a_valid_name_is_parsed_successfully() {
+        let name = "Ursula Le Guin".to_string();
+        assert_ok!(SubscriberName::parse(name));
+    }
+}
