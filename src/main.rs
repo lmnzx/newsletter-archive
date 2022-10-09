40f5393d0e71b93cb8e70f3496340fe0f44812c5
Sayan Mallick <snyxmk@gmail.com>
2022-10-09 13:00:24 +0530

    Persisting A New Subscriber

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
 use {
     axum::{
         body::Bytes,
-        handler::Handler,
         http::Request,
         middleware::{self},
         response::Response,
-        routing::get,
+        routing::{get, post},
         Router,
     },
+    sqlx::postgres::PgPoolOptions,
     std::{future::ready, net::SocketAddr, time::Duration},
     tower_http::{classify::ServerErrorsFailureClass, trace::TraceLayer},
     tracing::Span,
     tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt},
 };
 
-use newsletter::graceful_shutdown::shutdown_signal;
 use newsletter::metrics::{setup_metrics_recorder, track_metrics};
 use newsletter::routes::{global_404, health_check, subscriptions};
 
@@ -29,17 +28,22 @@ async fn main() {
 
     let recorder_handle = setup_metrics_recorder();
 
-    let app = Router::new()
+    let db_pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect_lazy("postgres://postgres:password@127.0.0.1:5432/newsletter")
+        .expect("can connect to database");
+
+    let app = Router::with_state(db_pool)
         .route("/health_check", get(health_check))
-        .route("/subscriptions", get(subscriptions))
-        .fallback(global_404.into_service())
+        .route("/subscriptions", post(subscriptions))
+        .fallback(global_404)
         .route("/metrics", get(move || ready(recorder_handle.render())))
         .route_layer(middleware::from_fn(track_metrics))
         .layer(TraceLayer::new_for_http())
         .layer(
             TraceLayer::new_for_http()
                 .on_request(|request: &Request<_>, _span: &Span| {
-                    tracing::debug!("started {} {}", request.method(), request.uri().path())
+                    tracing::debug!("st`arted {} {}", request.method(), request.uri().path())
                 })
                 .on_response(|_response: &Response, latency: Duration, _span: &Span| {
                     tracing::debug!("response generated in {:?}", latency)
@@ -58,7 +62,6 @@ async fn main() {
     tracing::debug!("listing on address {addr}");
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
-        .with_graceful_shutdown(shutdown_signal())
         .await
         .unwrap();
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,5 +1,35 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use {
+    axum::{
+        extract::{Form, State},
+        http::StatusCode,
+        response::IntoResponse,
+    },
+    chrono::Utc,
+    sqlx::postgres::PgPool,
+    uuid::Uuid,
+};
 
-pub async fn subscriptions() -> impl IntoResponse {
+#[derive(serde::Deserialize, Debug)]
+pub struct FormData {
+    email: String,
+    name: String,
+}
+
+pub async fn subscriptions(
+    State(db_pool): State<PgPool>,
+    Form(form_data): Form<FormData>,
+) -> impl IntoResponse {
+    tracing::debug!("{:#?}", form_data);
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form_data.email,
+        form_data.name,
+        Utc::now()
+    )
+    .execute(&db_pool)
+    .await;
     (StatusCode::OK, "subscriptions endpoint")
 }
