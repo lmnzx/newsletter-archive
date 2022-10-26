8aaba4e102e4a8422ad1687fb1750299006071cf
Sayan Mallick <snyxmk@gmail.com>
2022-10-26 10:19:52 +0530

    Saving a new user to the database

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
-use newsletter::run;
+use newsletter::configuration::get_configuration;
+use newsletter::startup::run;
+use sqlx::PgPool;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run(std::net::TcpListener::bind("127.0.0.1:8000")?)?.await
+    let config = get_configuration().expect("Failed to read configuration");
+
+    let connection = PgPool::connect(&config.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+
+    let address = format!("127.0.0.1:{}", config.application_port);
+
+    let listener = TcpListener::bind(address)?;
+
+    run(listener, connection)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,13 +1,31 @@
+use newsletter::configuration::get_configuration;
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
+
+// TODO Fix Testing
+
+fn spawn_app() -> String {
+    // Retrieve the port assigned to us by the OS
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
+    let port = listener.local_addr().unwrap().port();
+
+    let server = newsletter::startup::run(listener).expect("Failed to bind address");
+
+    let _ = tokio::spawn(server);
+
+    // Return the application address to the caller
+    format!("http://127.0.0.1:{}", port)
+}
 
 #[tokio::test]
 async fn health_check_works() {
-    let address = spawn_app();
+    let app_address = spawn_app();
 
     let client = reqwest::Client::new();
 
     let response = client
-        .get(format!("{}/health_check", &address))
+        .get(format!("{}/health_check", &app_address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -16,16 +34,65 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() -> String {
-    // Retrieve the port assigned to us by the OS
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app_address = spawn_app();
 
-    let port = listener.local_addr().unwrap().port();
+    let client = reqwest::Client::new();
 
-    let server = newsletter::run(listener).expect("Failed to bind address");
+    let config = get_configuration().expect("Failed to read configuration");
+    let connection_string = config.database.connection_string();
 
-    let _ = tokio::spawn(server);
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres");
 
-    // Return the application address to the caller
-    format!("http://127.0.0.1:{}", port)
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
+}
+
+#[tokio::test]
+async fn subscribe_return_a_400_when_data_is_missing() {
+    let app_address = spawn_app();
+
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        )
+    }
 }
